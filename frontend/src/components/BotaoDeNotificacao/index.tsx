baa26d4c2165ba36dc720adbd9076836c9f902bb
Jairo Pinheiro Da Conceição Junior <jairopnhr@gmail.com>
2022-11-14 17:10:50 -0300

    Primeiro Componente

---
+++ frontend/src/components/BotaoDeNotificacao/index.tsx
@@ -0,0 +1,11 @@
+import icon from '../../assets/img/Vector.svg'
+import './style.css'
+function BotaoNotificacao()
+{
+    return(
+<div className="dsmeta-red-btn"> 
+    <img src={icon} alt='Notificar'/>
+</div>
+    )
+}
+export default BotaoNotificacao
